4c807c8518c1b664dc9d156dd1b3396f979bf21c
DaeYoungBae <bdy6290@naver.com>
2020-12-27 11:31:23 +0900

    Generate Field Form

---
+++ src/main/java/my/mynote/domain/Content.java
@@ -0,0 +1,42 @@
+package my.mynote.domain;
+
+public class Content {
+
+    private int id;
+    private String username;
+    private String title;
+    private String body;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+}
---
+++ src/main/java/my/mynote/domain/ContentForm.java
@@ -0,0 +1,33 @@
+package my.mynote.domain;
+
+public class ContentForm {
+
+    private String title;
+    private String body;
+    private String username;
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
